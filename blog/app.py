55482563330a3606dfb4faf4bd87afa33b1a0b40
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-26 13:42:03 +0100

    add cv

--- blog/app.py
+++ blog/app.py
@@ -1,9 +1,14 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    # retourne un template
+@app.route('/index')
+def cv():
+    # retourne un template page html
     return render_template('pages/index.html')
+@app.route('/home')
+def home():
+
+    return render_template('pages/home.html')
+    
 if __name__ == '__main__':
     app.run(debug = True,host = '0.0.0.0',port = '5000')
