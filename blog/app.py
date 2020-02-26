307c0a44bf7bfb8bc8664744e5ec51f63e8ab250
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-26 18:49:44 +0100

    add folder of errors

--- blog/app.py
+++ blog/app.py
@@ -19,5 +19,11 @@ def cv():
     # retourne un template page html
     return render_template('pages/index.html')
 
+""" gestion d'erreur 404 """
+@app.errorhandler(404)
+def page_not_found(error):
+
+    return render_template('errors/404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug = True,host = '0.0.0.0',port = '5000')
