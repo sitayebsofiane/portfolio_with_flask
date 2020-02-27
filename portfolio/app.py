5ee4baf53064705abca688cff7c19d91aad7b5e1
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-27 15:31:15 +0100

    add home text

--- portfolio/app.py
+++ portfolio/app.py
@@ -8,11 +8,6 @@ app = Flask(__name__)
 def home():
 
     return render_template('pages/home.html')
-
-@app.route('/about')
-def about():
-
-    return render_template('pages/about.html')
     
 @app.route('/cv')
 def cv():
