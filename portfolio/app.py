f244035c2191c2c3a7178cd05406043d8418a4bf
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-02-29 21:35:19 +0100

    add SQLAlchemy

--- portfolio/app.py
+++ portfolio/app.py
@@ -3,8 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:as122014@localhost/flask'
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def home():
