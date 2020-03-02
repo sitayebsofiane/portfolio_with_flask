8b54495eb220ba3c34bf21a4ef23a0cee74613b8
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-02 10:01:16 +0100

    add mail in contact me

--- portfolio/app.py
+++ portfolio/app.py
@@ -2,17 +2,19 @@ from flask import Flask,render_template,abort
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:as122014@localhost/flask'
+#--------------------------------------------------------conect to bdd---------------------------------------------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:as122014@localhost:5432/flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'bruno'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'username:{self.username} eamil{self.email} #####'
 
 @app.route('/')
 def home():
@@ -39,8 +41,11 @@ from post import Post
 
 @app.route('/blog/')
 def blog():
-   
-    return render_template('posts/index.html',bruno = Post.all())
+    # u =User(username ='emira',email ='emira')
+    # db.session.delete(u)
+    # db.session.commit()
+    tout = User.query.first()
+    return render_template('posts/index.html',bruno = Post.all(),tout = tout)
       
 #<id> c'est un parametre qui va etre reçu par la fonction
 @app.route('/blog/posts/<int:id>')
@@ -77,4 +82,5 @@ def page_not_found(error):
     return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug = True,host = '0.0.0.0',port = '5000')
