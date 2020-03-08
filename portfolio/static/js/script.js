5e4234283ab21a6ea90de8b0ed86bf343facc7de
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-08 10:36:31 +0100

    update js

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -8,6 +8,13 @@ function loop(){
     }else{
         clearTimeout(timeLooper);
     }
-    timeLooper=setTimeout('loop()',70);
+    timeLooper=setTimeout("loop()",70);
 };
 loop();
+// message d'erreur
+function cv(){
+var message = document.getElementById("message");
+message.textContent = "Aha !malin tu veut voir mon cv sans donner le nom de ton entreprise!";
+}
+var entreprise = document.getElementById("nom").nodeValue;
+alert(cv);
