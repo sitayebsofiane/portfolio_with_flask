a21a0d9800f388821e345efa4e7c904496c946e7
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-11 00:27:16 +0100

    update admin.html

--- portfolio/app.py
+++ portfolio/app.py
@@ -3,13 +3,13 @@ from portfolio.model.model import Model
 import hashlib
 import datetime
 model =Model('data')
-app = Flask(__name__)
+appli = Flask(__name__)
 
-@app.context_processor
+@appli.context_processor
 def inject_now():
     return dict(now=datetime.datetime.now().year)
 #-----------------------------------------admin------------------------------------------
-@app.route('/login',methods=['GET', 'POST'])
+@appli.route('/login',methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         pseudo = request.form['pseudo']
@@ -20,7 +20,7 @@ def login():
             return redirect(url_for('login'))
     return render_template('admin/login.html')
 
-@app.route('/admin',methods=['GET', 'POST'])
+@appli.route('/admin',methods=['GET', 'POST'])
 def admin():
     if request.method == 'POST':
         model.add_service(request.form.get('name_add'),request.form.get('techno'),request.form.get('outils'))
@@ -32,11 +32,11 @@ def admin():
 #---------------------------------------- end admin--------------------------------------
 
 #-----------------------------------------visitor--------------------------------------------
-@app.route('/')
+@appli.route('/')
 def home():
     return render_template('pages/home.html')
 
-@app.route('/service')
+@appli.route('/service')
 def service():
     #model.add_service('application bureau','JAVA,PYTHON','tikinter, JFrame')
     #model.delete_service('application bureau')
@@ -46,14 +46,13 @@ def service():
 
 #---------------------------------------------------error---------------------------------------
 """ gestion d'erreur 404 """
-@app.errorhandler(404)
+@appli.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
 #-----------------------------------------------end error---------------------------------------
 
 #-----------------------------------------------run---------------------------------------------
 if __name__ == '__main__':
-    app.secret_key = '2d9-E2.)f&é,A$p@fpa+zSU03êû9_'
-    app.run(debug = True)
+    appli.run(debug = True)
 #--------------------------------------------end run--------------------------------------------
 
